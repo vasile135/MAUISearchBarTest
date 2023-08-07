0363a89b4ba2fd48c3f2697118c67e19d5ce4d16
hetzner <primtechMD@outlook.com>
2023-08-07 13:50:38 +0200

    Initial commit

---
+++ src/SearchBarTest/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SearchBarTest;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ src/SearchBarTest/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace SearchBarTest;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    void SearchBar_Focused(System.Object sender, Microsoft.Maui.Controls.FocusEventArgs e)
+    {
+        DisplayAlert("Focused", "Focused", "OK");
+    }
+
+    void SearchBar_Unfocused(System.Object sender, Microsoft.Maui.Controls.FocusEventArgs e)
+    {
+        DisplayAlert("Unfocused", "Unfocused", "OK");
+    }
+}
+
+
